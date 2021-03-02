39c2177de6226a139eb13ca360a988c3c98e73be
repl.it user <replituser@example.com>
2021-03-02 11:10:59 +0000

    If statements

---
+++ Demos/ifelseifelse.py
@@ -0,0 +1,17 @@
+print("What is your name?")
+n = input()
+# print("Do you have a dog? (type True or False)")
+# dog = input()
+#bool is boolean datatype - only stores True/False
+
+
+if len(n) >= 9: # allow length of exactly 9 and greater
+  print("You have a very loooong name!")
+  print ("Your name contains {} letters".format(len(n)))
+elif len(n) > 6:
+  print("Your name is a bit long. Consider a nickname")
+elif len(n) > 3:
+  print("your name is veeeery short")
+else:
+  print("Your name is quite okay")
+print("This is the END of the program!")
