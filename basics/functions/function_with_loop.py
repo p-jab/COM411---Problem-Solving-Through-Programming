df53b6de8a10aa88bf0dca08c1d31b34b02cfdc5
repl.it user <replituser@example.com>
2021-03-11 10:57:49 +0000

    Sol3 Added

---
+++ basics/functions/function_with_loop.py
@@ -0,0 +1,10 @@
+def crossed_bridge(steps):
+  # Display each step 
+  for step in range(steps):
+    print("Crossed step.") 
+
+  # Display message
+  if (steps > 5):
+    print("The bridge is collapsing!")
+  else: 
+    print("We must keep going!")
--- main.py
+++ main.py
@@ -1,30 +1,24 @@
-#Program that displays a menu and allows the user to either see a nice message, calculate areas of rectangle or triangle or display a times table for a number.
+#Defining my own function to calculate area of a triangle
+def calculate_area(h = 10,b = 5):
+  area = 0.5*h*b
+  return area
 
-print("Please choose and option from the menu:\n\n1-Nice message\n2-Area of a rectangle\n3-Area of triangle\n4-Times table")
+def run():
+  x = calculate_area(4)
+  x += 1
+  print(f"The area of triangle is {x}")
 
-option = int(input())
+#Call for the function
+run()
 
-if option == 1:
-  print("Today will be a good day!")
-elif option == 2:
-  print("Enter the length of the rectangle:")
-  l = int(input())
-  print("Enter the width of the rectangle")
-  w = int(input())
-  area = w*l
-  print("The area of this rectangle was {}".format(area))
-elif option == 3:
-  print("Enter the base of the triangle:")
-  base = float(input())
-  print("Enter the height of the triangle")
-  height = float(input())
-  area = 0.5*base*height
-  print("The area of this triangle was {:.2f}".format(area))
-elif option == 4:
-  print("What number would you like to see times table for?")
-  n = int(input())
-  for i in range(1,11,1):
-    print("{}x{} = {}".format(n,i,n*i))
-else:
-  print("There is no such option. Go to Specsavers!")
 
+
+# calculate_area(5)
+# calculate_area(b=20)
+# calculate_area(13,7)
+
+
+
+
+
+#Paremeter - a value that you plug into a function
